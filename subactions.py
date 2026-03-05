c4ededc344ba3b7265a9c04978b465b241d9e615
OzzysMess <oaldaz@uw.edu>
2026-03-04 20:21:52 -0800

    stuff

--- subactions.py
+++ subactions.py
@@ -2,60 +2,98 @@
 # a cave, etc.
 
 
-def activate_OncePerRound_ability():
+
+
+def activate_OncePerRound_ability(cave_card, player_hand, player_mat):
+    # search for once per round ability in dragon cards
     return
 
-def activate_WhenPlayed_ability():
+def activate_WhenPlayed_ability(cave_card, player_hand, player_mat):
+    # search for when played ability in dragon cards
     return
 
-def cache_any_resource_from_gen_supply():
+def cache_any_resource_from_gen_supply(cave_card, player_hand, player_mat):
+    # search for when resourcecached ability in dragon cards
     return
 
-def entice_discounted_dragon():
+def entice_discounted_dragon(cave_card, player_hand, player_mat):
     return
 
-def gain_any_resource():
+def gain_any_resource(cave_card, player_hand, player_mat):
+    print("Choose a resource to gain:")
+    print("1. Crystal")
+    print("2. Gold")
+    print("3. Meat")
+    print("4. Milk")
+    choice = input("Enter the number of the resource you want to gain: ")
+    if choice == "1":
+        gain_crystal(cave_card, player_hand, player_mat)
+    elif choice == "2":
+        gain_gold(cave_card, player_hand, player_mat)
+    elif choice == "3":
+        gain_meat(cave_card, player_hand, player_mat)
+    elif choice == "4":
+        gain_milk(cave_card, player_hand, player_mat)
+    else:
+        print("Invalid choice, no resource gained.")
     return
 
-def gain_cave_benifit(benefits):
+def gain_benefit(cave_card, player_hand, player_mat):
     '''Choice of Dragon Card, Cave Card, or Any Resource'''
+    from init import ACTION_REGISTRY
+
+    print("Choose a benefit to gain:")
+    for i, benefit in enumerate(cave_card["benefits"], start=1):
+        print(f"{i}. {benefit}")
+    choice = input("Enter the number of the benefit you want to gain: ")
+    do_benefit  = ACTION_REGISTRY[cave_card["benefits"][int(choice)-1]]
+    do_benefit(cave_card, player_hand, player_mat)
     return
 
-def gain_cave_card():
+def gain_cave_card(cave_card, player_hand, player_mat):
     return
 
-def gain_coin():
+def gain_coin(cave_card, player_hand, player_mat):
+    player_hand["coins"] += 1
     return
 
-def gain_crystal():
+def gain_crystal(cave_card, player_hand, player_mat):
+    player_hand["crystals"] += 1
     return
 
-def gain_dragon_card():
+def gain_dragon_card(cave_card, player_hand, player_mat):
     return
 
-def gain_dragon_guild():
+def gain_dragon_guild(cave_card, player_hand, player_mat):
+    global guildtrack
+
     return
 
-def gain_gold():
+def gain_gold(cave_card, player_hand, player_mat):
+    player_hand["gold"] += 1
     return
 
-def gain_meat():
+def gain_meat(cave_card, player_hand, player_mat):
+    player_hand["meat"] += 1
     return
 
-def gain_milk():
+def gain_milk(cave_card, player_hand, player_mat):
+    player_hand["milk"] += 1
     return
 
-def lay_egg():
+def lay_egg(cave_card, player_hand, player_mat):
+    # TODO Implement egg-laying logic, including checking for max eggs and updating player mat
+    player_hand["eggs"] += 1
     return
 
-def offer_to_pay_and_play_cave_card():
+def offer_to_pay_and_play_cave_card(cave_card, player_hand, player_mat):
     return
 
-def offer_3x():
+def offer_3x(cave_card, player_hand, player_mat):
     return
 
-def swap_dragon_locations():
+def swap_dragon_locations(cave_card, player_hand, player_mat):
     return
 
-def tuck_dragon_card_from_deck():
+def tuck_dragon_card_from_deck(cave_card, player_hand, player_mat):
     return
